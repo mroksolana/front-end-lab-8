215faee56267e31c547602924364049d3ddb8616
Roksolana Havryliv <roksolana6497@gmail.com>
2018-02-07 12:52:55 +0200

    Add files via upload

--- homework_06/js/task2.js
+++ homework_06/js/task2.js
@@ -1,11 +1,11 @@
-var a = parseInt (prompt ("Enter amount of EURO:"))
-var b = parseInt (prompt ("Enter amount of USD:"))
+var a = parseInt(prompt("Enter amount of EURO:")),
+    b = parseInt(prompt("Enter amount of USD:"))
 
-const euro=34.5116, usd=27.7424
+const euro = 34.5116,
+    usd = 27.7424
 
-var euro_conv =  euro * a
-var usd_conv = usd * b
-var z = euro / usd
+var euro_conv = euro * a,
+    usd_conv = usd * b,
+    z = euro / usd
 
-console.log( a + " euros are equal "+ euro_conv.toFixed(2) + " UAH,  "  + b + " dollars are equal " + usd_conv.toFixed(2) + " UAH, one euro is equal " + z.toFixed(3) + " dollars.")
-
+console.log(a + " euros are equal " + euro_conv.toFixed(2) + " UAH,  " + b + " dollars are equal " + usd_conv.toFixed(2) + " UAH, one euro is equal " + z.toFixed(3) + " dollars.")
