70119d0aa8724ac9c8c47e9056b69918aa862a4e
Roksolana Havryliv <roksolana6497@gmail.com>
2018-02-08 18:53:49 +0200

    Update task2.js

--- homework_06/js/task2.js
+++ homework_06/js/task2.js
@@ -1,11 +1,11 @@
 var a = parseInt(prompt("Enter amount of EURO:")),
-    b = parseInt(prompt("Enter amount of USD:"))
+    b = parseInt(prompt("Enter amount of USD:"));
 
 const euro = 34.5116,
-    usd = 27.7424
+    usd = 27.7424;
 
 var euro_conv = euro * a,
     usd_conv = usd * b,
-    z = euro / usd
+    z = euro / usd;
 
-console.log(a + " euros are equal " + euro_conv.toFixed(2) + " UAH,  " + b + " dollars are equal " + usd_conv.toFixed(2) + " UAH, one euro is equal " + z.toFixed(3) + " dollars.")
+console.log(a + " euros are equal " + euro_conv.toFixed(2) + " UAH,  " + b + " dollars are equal " + usd_conv.toFixed(2) + " UAH, one euro is equal " + z.toFixed(3) + " dollars.");
