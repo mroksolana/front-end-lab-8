57d21b2a972ed930c12de2431383b4ccc12248e0
Roksolana Havryliv <roksolana6497@gmail.com>
2018-02-13 15:13:38 +0200

    Add files via upload

--- homework_07/js/task1.js
+++ homework_07/js/task1.js
@@ -1,32 +1,22 @@
-var n = prompt("Please, enter natural number, which is less than 21:", 20);
-var i, j, k;
-
-if (n>0 && n<=20) { 
-
-for (i=1; i<=n; i++)
-{
-    for (j=i; j<n; j++)
-    {
-        console.log(" ");
-    }
-    for (k=1; k<(i*2); k++)
-    {
-        console.log("[~]");
-    }
-    console.log('\n');
-}
+var n = Number(prompt("Please, enter natural number, which is less than 21:", 20));
+var i, j, k, param, colSpace, colStar;
+var textmessage = '';
 
 
+if (n > 0 && n <= 20) {
 
-
-
-
-
-
-
-
-
-
+	for (i = 1; i <= n; i++) {
+		for (colSpace = 1; colSpace <= n - i; colSpace++) {
+			textmessage += " " + " " + " ";
+		}
+		for (colStar = 1; colStar <= i; colStar++) {
+			textmessage += "[~]";
+			if (colStar % i != 0) {
+				textmessage += "[~]"
+			}
+		}
+		textmessage += "\n";
+	}
+    console.log(textmessage);
+    
 } else console.error("Incorect Date")
-
-
