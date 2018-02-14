0765f43a3e4af336c17cb551647701c896dbad70
Roksolana Havryliv <roksolana6497@gmail.com>
2018-02-14 17:48:14 +0200

    Add files via upload

---
+++ homework_08/js/isBigger.js
@@ -0,0 +1,10 @@
+var numberFirst = Number(prompt ("Enter 2 Numbers to compare. The first:", )),
+numberSecond = Number(prompt ("The second:", )),
+result = true;
+
+function isBigger () {
+   var compare =  numberFirst > numberSecond;
+   return result;
+   console.log (result);
+
+}
