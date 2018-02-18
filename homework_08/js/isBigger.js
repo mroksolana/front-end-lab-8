effe8f5fb488181c085b4306512ed9acddb3de77
Roksolana Havryliv <roksolana6497@gmail.com>
2018-02-18 15:40:24 +0200

    Update isBigger.js

--- homework_08/js/isBigger.js
+++ homework_08/js/isBigger.js
@@ -1,10 +1,3 @@
-var numberFirst = Number(prompt ("Enter 2 Numbers to compare. The first:", )),
-numberSecond = Number(prompt ("The second:", )),
-result = true;
-
-function isBigger () {
-   var compare =  numberFirst > numberSecond;
-   return result;
-   console.log (result);
-
+function isBigger(firstNumber, secondNumber) {
+	return firstNumber > secondNumber;
 }
