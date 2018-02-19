8e4a531d3b9957413856a22d6029d6cf8a2375f4
Roksolana Havryliv <roksolana6497@gmail.com>
2018-02-19 18:59:50 +0200

    Update getTransformedArray.js

--- homework_09/js/getTransformedArray.js
+++ homework_09/js/getTransformedArray.js
@@ -1,15 +1,12 @@
-function increment (myNumber){
-    return myNumber+1;
+function increment(myNumber) {
+	return myNumber + 1;
 }
 
 
-function getTransformedArray (Array, increment)
-{
-    let newArray = [];
-    forEach(myArray, function(item, index) {
-        newArr[index] = increment(item);
-    });
-    return newArr;
-    }
-
- 
+function getTransformedArray(Array, increment) {
+	let newArray = [];
+	forEach(myArray, function (item, index) {
+		newArr[index] = increment(item);
+	});
+	return newArray;
+}
