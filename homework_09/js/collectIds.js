6be0020171b0d09bc1c9599ecad231fe6a003e6e
Roksolana Havryliv <roksolana6497@gmail.com>
2018-02-20 00:26:44 +0200

    Update collectIds.js

--- homework_09/js/collectIds.js
+++ homework_09/js/collectIds.js
@@ -1,6 +1,6 @@
 function collectIds(movies) {
 	let filtred = getFiltredArray(movies, function (R) {
-		return R.rating > 3.0;
+		return (R.rating > 3.0);
 	});
 
 	return getTransformedArray(filtred, function (R) {
