b42d5c3e5a3358af82f25462345cca988d3e6d93
Roksolana Havryliv <roksolana6497@gmail.com>
2018-02-27 23:41:24 +0200

    formated app.js

--- homework_11/app.js
+++ homework_11/app.js
@@ -1,57 +1,57 @@
 const rootNode = document.getElementById("root");
 
 
-function createTree (structure) { 
-    
-let newUl = document.createElement("ul");
+function createTree(structure) {
 
-for (let i = 0; i < structure.length; i++) {
-    let newLi = document.createElement("li");
-    let newIcon = document.createElement("i");
-    let newParag = document.createElement("p");
-    let newDiv = document.createElement("div");
+	let newUl = document.createElement("ul");
 
-    newIcon.className = "material-icons";
-    
-    newLi.appendChild(newDiv);
-    newUl.appendChild(newLi);
-    newDiv.appendChild(newIcon);
-    newDiv.appendChild(newParag);
-   
-    if (structure[i].folder) {
-        newIcon.innerHTML = "folder";
-        newParag.addEventListener("onclick", openTree);
+	for (let i = 0; i < structure.length; i++) {
+		let newLi = document.createElement("li");
+		let newIcon = document.createElement("i");
+		let newParag = document.createElement("p");
+		let newDiv = document.createElement("div");
 
-        if (structure[i].children) {
-            createTree(structure[i].children, newLi); 
-        } else {
-                let newParag = document.createElement("newParag");
-                newParag.mainContent = "Folder is empty";
-                newLi.appendChild(newParag);
-                newParag.classList.add("emptyFolder");
-            }
-        } else {
-            newIcon.innerHTML = "insert_drive_file";
-            newIcon.classList.add("file");
-        }
+		newIcon.className = "material-icons";
 
-    }
+		newLi.appendChild(newDiv);
+		newUl.appendChild(newLi);
+		newDiv.appendChild(newIcon);
+		newDiv.appendChild(newParag);
+
+		if (structure[i].folder) {
+			newIcon.innerHTML = "folder";
+			newParag.addEventListener("onclick", openTree);
+
+			if (structure[i].children) {
+				createTree(structure[i].children, newLi);
+			} else {
+				let newParag = document.createElement("newParag");
+				newParag.mainContent = "Folder is empty";
+				newLi.appendChild(newParag);
+				newParag.classList.add("emptyFolder");
+			}
+		} else {
+			newIcon.innerHTML = "insert_drive_file";
+			newIcon.classList.add("file");
+		}
+
+	}
 
 }
 
 
-function openTree () {
-    let datum = this.nextElementSibling;
-    if (datum) {
-        let newIcon = this.firstChild;
-        if (datum.style.display == "block") {
-            datum.style.display == "none";
-            newIcon.innerHTML = "folder"
-        } else {
-            datum.style.display = "block";
-            newIcon.innerHTML = "folder_open";
-        }
-      }
+function openTree() {
+	let datum = this.nextElementSibling;
+	if (datum) {
+		let newIcon = this.firstChild;
+		if (datum.style.display == "block") {
+			datum.style.display == "none";
+			newIcon.innerHTML = "folder"
+		} else {
+			datum.style.display = "block";
+			newIcon.innerHTML = "folder_open";
+		}
+	}
 }
 
 
